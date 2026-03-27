3e5f785af586b3274478d4f0de1bb845f8553313
chloeejp <hoiteenj@gmail.com>
2026-03-27 21:53:33 +0700

    Create DoubleTapToLockPreference.java

---
+++ src/com/android/launcher3/lineage/DoubleTapToLockPreference.java
@@ -0,0 +1,6 @@
+// src/com/android/launcher3/lineage/DoubleTapToLockPreference.java
+package com.android.launcher3.lineage;
+
+public class DoubleTapToLockPreference {
+    public static final String KEY_DOUBLE_TAP_TO_LOCK = "pref_double_tap_to_lock";
+}
